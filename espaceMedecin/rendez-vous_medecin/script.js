64e3b2a748006026dcf99f7d85d33dd106af802a
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2022-06-20 08:56:24 +0100

    RendezVousMedecin

---
+++ espaceMedecin/rendez-vous_medecin/script.js
@@ -0,0 +1,7 @@
+
+function toggle(){
+    var blur=document.getElementById("blur");
+    blur.classList.toggle('active');
+    var boite = document.getElementById('boite');
+    boite.classList.toggle('active');
+}
