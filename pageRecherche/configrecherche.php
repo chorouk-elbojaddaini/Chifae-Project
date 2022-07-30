2c3358f53dd557b923b9f2fb244141e57adfbc68
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2022-07-30 02:08:42 +0100

    RechercheP

--- pageRecherche/configrecherche.php
+++ pageRecherche/configrecherche.php
@@ -5,7 +5,6 @@
         //   $ville = $_SESSION['ville'] ;
         //   $specialite = $_SESSION['specialite'] ;
         // Start pagination
-        
         $perPage =10 ; 
         $page = isset($_GET["page"]) ? (int)$_GET["page"] : 1;
         $start = ($page > 1 ) ? ($page * $perPage) -$perPage : 0;
@@ -14,62 +13,49 @@
         $result3 = mysqli_query($conn, "SELECT * FROM medecin WHERE (specialite= '$_SESSION[specialite]' AND ville='$_SESSION[ville]' ) OR ( ( nom = '$_SESSION[nom]' OR prenom = '$_SESSION[nom]'  OR CONCAT(CONCAT(nom,' '),prenom) LIKE '%$_SESSION[nom]%') AND ville='$_SESSION[ville2]' )  ");
         $total = mysqli_num_rows($result3);
         $pages = ceil($total/$perPage);
-
-        $result = mysqli_query($conn,"select * from medecin WHERE (specialite= '$_SESSION[specialite]' AND ville='$_SESSION[ville]' ) OR ( ( nom = '$_SESSION[nom]' OR prenom = '$_SESSION[nom]'  OR CONCAT(CONCAT(nom,' '),prenom) LIKE '%$_SESSION[nom]%') AND ville='$_SESSION[ville2]' )    lIMIT  $start , $perPage ");
-        $resultArray = array();
-        //fetch data one by one
-        while($item = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-            $resultArray[] = $item;
-        }
-        
-        
         // End pagination
         // $query="SELECT * FROM medecin WHERE specialite= '$_SESSION[specialite]' AND ville='$_SESSION[ville]'";
         // $result= mysqli_query($conn,$query);
         // $queryResults = mysqli_num_rows($result);
         if ($total > 0){
-          $u=0;
+          $i=0;
           while($row=mysqli_fetch_array($result2)){
 
            $b = $i++;
-           
-      
-          // echo $resultArray[$i]['nom'];
-          if($resultArray[$u]['photo'] != null){ 
-            echo' <div class="medecin-info info1" id="'."$row[id]".'">
-          <div class="div-photo">';
-           ?>
-            
-            <img id="photo"  src="data:image;base64,<?php echo $resultArray[$u]['photo'] ;?>" class="img-medecin med1">
-          
-         
-       <?php  $u++;}
-        else{
-          
-          if("$row[sexe]" === "femme"){
-            echo' <div class="medecin-info info1" id="'."$row[id]".'" >
-        <div class="div-photo">
-        <img
-        src="assets/medecinfemme.jpg"
-        alt="medecin femme"
-        class="img-medecin"
-        
-      />';
-          }
-          else{
-            
-          echo' <div class="medecin-info info1" id="'."$row[id]".'" >
+          if(empty("$row[photo]")==true){
+            if("$row[sexe]" === "femme"){
+              echo' <div class="medecin-info info1" id="'."$row[id]".'" >
           <div class="div-photo">
           <img
-          src="assets/medecinhomme.jpg"
-          alt="medecin "
+          src="assets/medecinfemme.jpg"
+          alt="medecin femme"
           class="img-medecin"
           
         />';
-          } 
-             }
-        
- 
+            }
+            else{
+              
+            echo' <div class="medecin-info info1" id="'."$row[id]".'" >
+            <div class="div-photo">
+            <img
+            src="assets/medecinhomme.jpg"
+            alt="medecin "
+            class="img-medecin"
+            
+          />';
+            }
+          }
+          else{
+            echo' <div class="medecin-info info1" id="'."$row[id]".'">
+          <div class="div-photo">
+          <img
+          src="data:image/jpg;base64,' . $row['photo']. '"
+          alt="medecin"
+          class="img-medecin med1"
+        />';
+
+          }
+          
           echo '<div class="voirplus plus1">
           <button class="btn-voirplus btn1 voirprof" id="'."$row[id]".'">voir  profil</button>
         </div>
